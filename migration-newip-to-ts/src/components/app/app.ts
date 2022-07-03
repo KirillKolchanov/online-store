8b8368a617ab36df9aa0d2505eb6c5fca4869223
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-07-03 14:37:29 +0200

    fix: add ts types to app.ts

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -1,16 +1,17 @@
 import AppController from '../controller/controller';
+import { NewsResponse } from '../types/news';
 import { AppView } from '../view/appView';
 
 class App {
-  constructor() {
-    this.controller = new AppController();
-    this.view = new AppView();
-  }
+  controller = new AppController();
+  view = new AppView();
 
   start() {
-    document.querySelector('.sources')
-      .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-    this.controller.getSources((data) => this.view.drawSources(data));
+    document.querySelector('.sources')?.addEventListener(
+      'click',
+      (e) => this.controller.getNews(e, (news: NewsResponse) => this.view.drawNews(news))
+    );
+    this.controller.getSources((sources) => this.view.drawSources(sources));
   }
 }
 
