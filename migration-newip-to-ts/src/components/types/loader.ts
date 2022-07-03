e3fe307e2883b49215bb91a95511513bb41fadb8
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-07-03 14:32:11 +0200

    feat: add types folder and files with TS files

---
+++ migration-newip-to-ts/src/components/types/loader.ts
@@ -0,0 +1,11 @@
+import { SourcesResponse } from './source';
+
+export interface LoaderOptions {
+  apiKey: string;
+}
+
+export interface LoaderRequestOptions {
+  sources?: string
+}
+
+export type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS'
