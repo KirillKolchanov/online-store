618cc8a93201508b3e9c521b6409e1cb877755b4
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-07-12 00:26:41 +0200

    feat: add base js generation of cards, add cars data base

---
+++ online-store/src/index.js
@@ -0,0 +1,33 @@
+import allCars from './components/db-cars/cars.js';
+
+const carsContainer = document.querySelector(".cars-container");
+
+function generateCard(obj) {
+  return `<div class="cars-card">
+  <div class="cars-card-img">
+    <img class="rounded" src="${obj.img}" alt="">
+  </div>
+  <h6 class="cars-card-title text-center">${obj.make} ${obj.model}</h6>
+  <div class="cars-card-price">
+    <h6 class="cars-card-price-header">Price</h6>
+    <h5 class="cars-card-price-number"><i class="bi bi-currency-dollar"></i> ${obj.price}</h5>
+  </div>
+  <div class="cars-card-description">
+    <div class="cars-description">
+      <p class="cars-card-year"><i class="bi bi-calendar"></i> ${obj.age}</p>
+      <p class="cars-card-fuel"><i class="bi bi-droplet"></i> ${obj.fuel}</p>
+    </div>
+    <div class="cars-description">
+      <p class="cars-card-miles"><i class="bi bi-speedometer2"></i> ${obj.miles}</p>
+      <p class="cars-card-city"><i class="bi bi-geo-alt"></i> ${obj.city}</p>
+    </div>
+    <div class="cars-card-submit text-center">
+      <button class="btn btn-outline-success cars-submit-button">Add to cart</button>
+    </div>
+  </div>
+</div>`
+}
+
+for (let i = 0; i < allCars.length; i++) {
+  carsContainer.insertAdjacentHTML('beforeend', generateCard(allCars[i]));
+}
