d08534f2ed5cf5a7f3decee554aad5faf7b6b746
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-07-13 00:19:45 +0200

    feat: add reset buttons to filters, add sorting engine

--- online-store/src/components/db-cars/cars.js
+++ online-store/src/components/db-cars/cars.js
@@ -1,43 +1,43 @@
 const allCars = [
   {
-    "img": "./assets/img/cars/ferrari-488.jpeg",
-    "make": "Ferrari",
-    "model": "488 Gtb 2dr Auto",
-    "price": "394,369",
-    "age": "2020",
-    "fuel": "petrol",
-    "miles": "2,274",
-    "city": "Warsaw"
+    img: './assets/img/cars/ferrari-488.jpeg',
+    make: 'Ferrari',
+    model: '488 Gtb 2dr Auto',
+    price: 394.369,
+    age: 2020,
+    fuel: 'petrol',
+    miles: '2,274',
+    city: 'Warsaw'
   },
   {
-    "img": "./assets/img/cars/Mercedes.jpeg",
-    "make": "Mercedes",
-    "model": "SLS SLS AMG",
-    "price": "203,188",
-    "age": "2011",
-    "fuel": "petrol",
-    "miles": "24,000",
-    "city": "Berlin"
+    img: './assets/img/cars/Mercedes.jpeg',
+    make: 'Mercedes',
+    model: 'SLS 63 AMG',
+    price: 203.188,
+    age: 2011,
+    fuel: 'petrol',
+    miles: '24,000',
+    city: 'Berlin'
   },
   {
-    "img": "./assets/img/cars/aston-martin.jpeg",
-    "make": "Aston Martin",
-    "model": "DBS V12 Superleggera",
-    "price": "298,638",
-    "age": "2022",
-    "fuel": "petrol",
-    "miles": "205",
-    "city": "London"
+    img: './assets/img/cars/aston-martin.jpeg',
+    make: 'Aston Martin',
+    model: 'DBS V12 Superleggera',
+    price: 298.638,
+    age: 2022,
+    fuel: 'petrol',
+    miles: 205,
+    city: 'London'
   },
   {
-    "img": "./assets/img/cars/Porshe 911.jpeg",
-    "make": "Porsche",
-    "model": "911 Turbo Coupe S",
-    "price": "232,966",
-    "age": "2020",
-    "fuel": "petrol",
-    "miles": "19,025",
-    "city": "Krakow"
+    img: './assets/img/cars/Porshe 911.jpeg',
+    make: 'Porsche',
+    model: '911 Turbo Coupe S',
+    price: 232.966,
+    age: 2020,
+    fuel: 'petrol',
+    miles: '19,025',
+    city: 'Krakow'
   },
 ]
 
--- online-store/src/index.js
+++ online-store/src/index.js
@@ -28,6 +28,49 @@ function generateCard(obj) {
 </div>`
 }
 
+const sortedArrFromMinPrice = [...allCars.sort((a,b) => a.price - b.price)];
+const sortedArrFromMaxPrice = [...allCars.sort((a,b) => b.price - a.price)];
+const sortedArrNewest = [...allCars.sort((a,b) => b.age - a.age)];
+const sortedArrOldest = [...allCars.sort((a,b) => a.age - b.age)];
+
 for (let i = 0; i < allCars.length; i++) {
-  carsContainer.insertAdjacentHTML('beforeend', generateCard(allCars[i]));
+  carsContainer.insertAdjacentHTML('beforeend', generateCard(sortedArrFromMinPrice[i]));
 }
+
+const sorting = document.querySelector(".form-control-sm");
+
+sorting.addEventListener("change", () => {
+  carsContainer.innerHTML = '';
+  if (sorting.value === "Low to High") {
+    for (let i = 0; i < allCars.length; i++) {
+      carsContainer.insertAdjacentHTML('beforeend', generateCard(sortedArrFromMinPrice[i]));
+    }
+  }
+
+  if (sorting.value === "High to Low") {
+    for (let i = 0; i < allCars.length; i++) {
+      carsContainer.insertAdjacentHTML('beforeend', generateCard(sortedArrFromMaxPrice[i]));
+    }
+  }
+
+  if (sorting.value === "Newest First") {
+    for (let i = 0; i < allCars.length; i++) {
+      carsContainer.insertAdjacentHTML('beforeend', generateCard(sortedArrNewest[i]));
+    }
+  }
+
+  if (sorting.value === "Oldest First") {
+    for (let i = 0; i < allCars.length; i++) {
+      carsContainer.insertAdjacentHTML('beforeend', generateCard(sortedArrOldest[i]));
+    }
+  }
+
+})
+
+// const priceInput = document.querySelector(".filter-price-input");
+
+// console.log(priceInput);
+
+// priceInput.addEventListener("input", () => {
+//   console.log(priceInput.value);
+// })
